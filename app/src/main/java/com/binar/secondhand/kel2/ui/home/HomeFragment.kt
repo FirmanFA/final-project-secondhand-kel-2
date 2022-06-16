fc79fc45e11eaaf58bf027e729c4829ee9256995
firmanfa <fafirmansyah20@gmail.com>
2022-06-16 15:14:01 +0700

    feat: main bottom navbar

--- app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeFragment.kt
@@ -7,186 +7,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.Toast
 import com.binar.secondhand.kel2.R
-import com.binar.secondhand.kel2.data.api.model.seller.banner.get.GetBannerResponse
-import com.binar.secondhand.kel2.data.api.model.buyer.product.GetProductResponse
-import com.binar.secondhand.kel2.data.resource.Status
 import com.binar.secondhand.kel2.databinding.FragmentHomeBinding
 import com.binar.secondhand.kel2.ui.base.BaseFragment
-import com.google.android.material.tabs.TabLayout
-import org.koin.android.ext.android.getKoin
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
-    private val homeViewModel by viewModel<HomeViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        getKoin().setProperty(
-            "access_token",
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtYWlsa2VsMkBtYWlsLmNvbSI" +
-                    "sImlhdCI6MTY1NTgwODc4NX0.uCRi6CtzaaaoMNLLD8C8eYMrwnZx3aZAFgi2_Ey6o1w"
-        )
-
-        setUpSearchBarListener()
-        setUpObserver()
-
-        binding.tabHomeCategory.addTab(
-            binding.tabHomeCategory.newTab()
-                .setText("Semua")
-                .setIcon(R.drawable.ic_baseline_search_24)
-                .setId(-1)
-        )
-
-        homeViewModel.getHomeProduct()
-        homeViewModel.getHomeBanner()
-        homeViewModel.getHomeCategory()
-
-//        for (i in 0..3) {
-//            binding.tabHomeCategory.addTab(
-//                binding.tabHomeCategory.newTab()
-//                    .setText("tab ke $i")
-//                    .setIcon(R.drawable.ic_baseline_search_24).setId(i * 10)
-//                    .setTag("ini tag tab ke $i")
-//            )
-//
-//        }
-
-        binding.tabHomeCategory.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-//                Toast.makeText(context, "${tab?.tag}", Toast.LENGTH_SHORT).show()
-                //do filter product here
-                if (tab?.id == -1){
-                    homeViewModel.getHomeProduct()
-                }else{
-                    homeViewModel.getHomeProduct(categoriId = tab?.id)
-                }
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-
-        })
-    }
-
-    private fun setUpObserver() {
-        homeViewModel.getBannerResponse.observe(viewLifecycleOwner) {
-            when (it.status) {
-
-                Status.LOADING -> {}
-
-                Status.SUCCESS -> {
-
-                    when (it.data?.code()) {
-                        200 -> {
-                            val data = it.data.body()
-                            showHomeBanner(data)
-                        }
-                    }
-                }
-
-                Status.ERROR -> {}
-            }
-        }
-
-        homeViewModel.getCategoryResponse.observe(viewLifecycleOwner) {
-            when (it.status) {
-
-                Status.LOADING -> {}
-
-                Status.SUCCESS -> {
-
-                    when (it.data?.code()) {
-                        200 -> {
-
-                            val data = it.data.body()
-                            if (data != null) {
-                                for (category in data) {
-                                    binding.tabHomeCategory.addTab(
-                                        binding.tabHomeCategory.newTab()
-                                            .setText(category.name)
-                                            .setIcon(R.drawable.ic_baseline_search_24)
-                                            .setId(category.id)
-                                            .setTag(category.name)
-                                    )
-
-                                }
-                            }
-
-                        }
-                    }
-                }
-
-                Status.ERROR -> {}
-            }
-        }
-
-        homeViewModel.getHomeProductResponse.observe(viewLifecycleOwner) {
-            when (it.status) {
-
-                Status.LOADING -> {}
-
-                Status.SUCCESS -> {
-
-                    when (it.data?.code()) {
-                        200 -> {
-
-                            val data = it.data.body()
-                            showHomeProductList(data)
-
-                        }
-                    }
-                }
-
-                Status.ERROR -> {}
-            }
-        }
-    }
-
-    private fun showHomeBanner(data: GetBannerResponse?) {
-        val adapter = HomeBannerAdapter {
-            //onclick item
-        }
-
-        adapter.submitList(data)
-
-        binding.vpHomeBanner.adapter = adapter
-    }
-
-    private fun showHomeProductList(productResponse: GetProductResponse?) {
-        val adapter = HomeProductAdapter {
-            //onclick item
-        }
-
-        adapter.submitList(productResponse)
-
-        binding.rvHomeProduct.adapter = adapter
-
-    }
-
-
-    private fun setUpSearchBarListener() {
         binding.etSearch.setEndIconOnClickListener {
             //do search
-            searchProduct(binding.etSearch.editText?.text.toString())
         }
 
         binding.etSearch.editText?.setOnEditorActionListener { textView, i, keyEvent ->
 
-            if (i == EditorInfo.IME_ACTION_SEARCH) {
+            if (i == EditorInfo.IME_ACTION_SEARCH){
                 //do something with search
-                searchProduct(binding.etSearch.editText?.text.toString())
             }
 
             true
         }
-    }
 
-    private fun searchProduct(query: String) {
-        //TODO
     }
 
 
