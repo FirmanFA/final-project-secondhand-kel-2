83dde5a941662e33a6453765c5166c17a5b4429a
firmanfa <fafirmansyah20@gmail.com>
2022-06-16 15:14:01 +0700

    feat: main bottom navbar

--- app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeFragment.kt
@@ -7,11 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.Toast
 import com.binar.secondhand.kel2.R
 import com.binar.secondhand.kel2.databinding.FragmentHomeBinding
 import com.binar.secondhand.kel2.ui.base.BaseFragment
-import com.google.android.material.tabs.TabLayout
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
@@ -24,32 +22,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
 
         binding.etSearch.editText?.setOnEditorActionListener { textView, i, keyEvent ->
 
-            if (i == EditorInfo.IME_ACTION_SEARCH) {
+            if (i == EditorInfo.IME_ACTION_SEARCH){
                 //do something with search
             }
 
             true
         }
 
-        for (i in 0..3) {
-            binding.tabHomeCategory.addTab(
-                binding.tabHomeCategory.newTab()
-                    .setText("tab ke $i")
-                    .setIcon(R.drawable.ic_baseline_search_24).setId(i*10)
-                    .setTag("ini tag tab ke $i")
-            )
-
-        }
-
-        binding.tabHomeCategory.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                Toast.makeText(context, "${tab?.tag}", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-
-        })
     }
 
 
