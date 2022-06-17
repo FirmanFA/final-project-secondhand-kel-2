2d37a5b72d2c4940a8e2ff9855c49a0fc2a0a5ac
chiumanfelix <chiumanfelix@gmail.com>
2022-06-17 18:13:54 +0700

    added notification fragment (layout done (need revision?))
    model that does not available yet :
    - patch seller/order/id (req done) -> forbidden
    - get buyer/order/id -> null
    - put buyer/order/id (req done)-> forbidden
    - get history -> blank array

--- app/src/main/java/com/binar/secondhand/kel2/data/api/service/ApiHelper.kt
+++ app/src/main/java/com/binar/secondhand/kel2/data/api/service/ApiHelper.kt
@@ -1,4 +1,12 @@
 package com.binar.secondhand.kel2.data.api.service
 
-class ApiHelper {
+import com.binar.secondhand.kel2.data.api.model.auth.login.PostLoginRequest
+import com.binar.secondhand.kel2.data.api.model.auth.register.PostRegisterRequest
+
+class ApiHelper(val apiService: ApiService) {
+    suspend fun postLogin(request: PostLoginRequest) = apiService.postLogin(request)
+
+    suspend fun postRegister(request: PostRegisterRequest) = apiService.postRegister(request)
+
+//    suspend fun getAuth(token: String) = apiService.getAuth(token)
 }
