75b89337fad55d7ed03b2e4aabc221174f293179
firmanfa <fafirmansyah20@gmail.com>
2022-06-17 19:50:20 +0700

    feat: add banner adapter class name

--- app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeBannerAdapter.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeBannerAdapter.kt
@@ -5,19 +5,19 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.binar.secondhand.kel2.data.api.model.buyer.order.get.GetOrderResponse
+import com.binar.secondhand.kel2.data.api.model.seller.banner.get.GetBannerResponseItem
 import com.binar.secondhand.kel2.databinding.HomeProductListLayoutBinding
 
-class HomeBannerAdapter(private val onClick: (GetOrderResponse.GetOrderResponseItem.Product) -> Unit) :
-    ListAdapter<GetOrderResponse.GetOrderResponseItem.Product, HomeBannerAdapter.ViewHolder>(CommunityComparator()) {
+class HomeBannerAdapter(private val onClick: (GetBannerResponseItem) -> Unit) :
+    ListAdapter<GetBannerResponseItem, HomeBannerAdapter.ViewHolder>(CommunityComparator()) {
 
 
     class ViewHolder(private val binding: HomeProductListLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(
-            currentProduct: GetOrderResponse.GetOrderResponseItem.Product,
-            onClick: (GetOrderResponse.GetOrderResponseItem.Product) -> Unit
+            currentProduct: GetBannerResponseItem,
+            onClick: (GetBannerResponseItem) -> Unit
         ) {
 
 
@@ -25,12 +25,12 @@ class HomeBannerAdapter(private val onClick: (GetOrderResponse.GetOrderResponseI
 
     }
 
-    class CommunityComparator : DiffUtil.ItemCallback<GetOrderResponse.GetOrderResponseItem.Product>() {
-        override fun areItemsTheSame(oldItem: GetOrderResponse.GetOrderResponseItem.Product, newItem: GetOrderResponse.GetOrderResponseItem.Product): Boolean {
+    class CommunityComparator : DiffUtil.ItemCallback<GetBannerResponseItem>() {
+        override fun areItemsTheSame(oldItem: GetBannerResponseItem, newItem: GetBannerResponseItem): Boolean {
             return oldItem === newItem
         }
 
-        override fun areContentsTheSame(oldItem: GetOrderResponse.GetOrderResponseItem.Product, newItem: GetOrderResponse.GetOrderResponseItem.Product): Boolean {
+        override fun areContentsTheSame(oldItem: GetBannerResponseItem, newItem: GetBannerResponseItem): Boolean {
             return oldItem.hashCode() == newItem.hashCode()
         }
     }
