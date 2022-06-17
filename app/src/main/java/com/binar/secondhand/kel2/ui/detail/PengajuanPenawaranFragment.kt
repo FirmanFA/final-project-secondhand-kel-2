4c52b3e686e3d816236f287944f26e74eb7a3345
rayxxv <brayencfc@gmail.com>
2022-06-17 20:23:45 +0800

    bottom dialog

---
+++ app/src/main/java/com/binar/secondhand/kel2/ui/detail/PengajuanPenawaranFragment.kt
@@ -0,0 +1,14 @@
+package com.binar.secondhand.kel2.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.binar.secondhand.kel2.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class PengajuanPenawaranFragment : BottomSheetDialogFragment() {
+
+}
