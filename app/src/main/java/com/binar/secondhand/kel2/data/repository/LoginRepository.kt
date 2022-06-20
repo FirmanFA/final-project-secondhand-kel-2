e488a43098b20b72af7aa16de5f5a452e7fef5d7
wahyuananta <wahyuanantajr@gmail.com>
2022-06-20 19:56:56 +0700

    Login and Register

---
+++ app/src/main/java/com/binar/secondhand/kel2/data/repository/LoginRepository.kt
@@ -0,0 +1,13 @@
+package com.binar.secondhand.kel2.data.repository
+
+import com.binar.secondhand.kel2.data.api.model.auth.login.PostLoginRequest
+import com.binar.secondhand.kel2.data.api.model.auth.register.PostRegisterRequest
+import com.binar.secondhand.kel2.data.api.service.ApiHelper
+
+class LoginRepository(private val apiHelper: ApiHelper) {
+
+    suspend fun postLogin(request: PostLoginRequest) = apiHelper.postLogin(request)
+
+    suspend fun postRegister(request: PostRegisterRequest) = apiHelper.postRegister(request)
+
+}
--- app/src/main/java/com/binar/secondhand/kel2/di/RepositoryModule.kt
+++ app/src/main/java/com/binar/secondhand/kel2/di/RepositoryModule.kt
@@ -1,9 +1,14 @@
 package com.binar.secondhand.kel2.di
 
-import com.binar.secondhand.kel2.data.repository.Repository
+import com.binar.secondhand.kel2.data.repository.LoginRepository
+import com.binar.secondhand.kel2.data.repository.RegisterRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    singleOf(::Repository)
+
+    singleOf(::LoginRepository)
+
+    singleOf(::RegisterRepository)
+
 }
