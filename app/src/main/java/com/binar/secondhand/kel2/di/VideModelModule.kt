9b3b810a6264c076ae102d6b3bc31d629c25c102
firmanfa <fafirmansyah20@gmail.com>
2022-06-20 18:56:13 +0700

    feat: home basic feature

--- app/src/main/java/com/binar/secondhand/kel2/di/VideModelModule.kt
+++ app/src/main/java/com/binar/secondhand/kel2/di/VideModelModule.kt
@@ -1,6 +1,9 @@
 package com.binar.secondhand.kel2.di
 
+import com.binar.secondhand.kel2.ui.home.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
+    viewModelOf(::HomeViewModel)
 }
