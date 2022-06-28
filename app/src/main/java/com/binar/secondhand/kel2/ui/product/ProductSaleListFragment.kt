c0e8272404290bf62dab34a04fb2a01a4727fe75
firmanfa <fafirmansyah20@gmail.com>
2022-06-28 17:34:28 +0700

    feat: make product sale list fragment

---
+++ app/src/main/java/com/binar/secondhand/kel2/ui/product/ProductSaleListFragment.kt
@@ -0,0 +1,23 @@
+package com.binar.secondhand.kel2.ui.product
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.binar.secondhand.kel2.R
+import com.binar.secondhand.kel2.databinding.FragmentProductSaleListBinding
+import com.binar.secondhand.kel2.ui.base.BaseFragment
+
+
+class ProductSaleListFragment :
+    BaseFragment<FragmentProductSaleListBinding>(FragmentProductSaleListBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+}
