7e289275c36c21be3da41eef9b39879b551eb1c4
firmanfa <fafirmansyah20@gmail.com>
2022-06-28 17:55:06 +0700

    feat: loading shimmer for home product image load

--- app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeProductAdapter.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeProductAdapter.kt
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.binar.secondhand.kel2.data.api.model.buyer.product.GetProductResponseItem
 import com.binar.secondhand.kel2.databinding.HomeProductListLayoutBinding
 import com.bumptech.glide.Glide
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 
 class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) :
     ListAdapter<GetProductResponseItem, HomeProductAdapter.ViewHolder>(CommunityComparator()) {
@@ -23,7 +25,18 @@ class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) 
             binding.root.setOnClickListener {
                 onClick(currentGetProductResponseItem)
             }
+            val shimmer = Shimmer.AlphaHighlightBuilder()// The attributes for a ShimmerDrawable is set by this builder
+                .setDuration(1800) // how long the shimmering animation takes to do one full sweep
+                .setBaseAlpha(0.7f) //the alpha of the underlying children
+                .setHighlightAlpha(0.6f) // the shimmer alpha amount
+                .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+                .setAutoStart(true)
+                .build()
+            val shimmerDrawable = ShimmerDrawable().apply {
+                setShimmer(shimmer)
+            }
             Glide.with(binding.root).load(currentGetProductResponseItem.imageUrl)
+                .placeholder(shimmerDrawable)
                 .into(binding.imvProductImage)
             binding.tvProductName.text = currentGetProductResponseItem.name
             binding.tvProductCategory.text = currentGetProductResponseItem.categories?.joinToString{
