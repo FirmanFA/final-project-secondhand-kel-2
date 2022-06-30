1ff982f33378a4cfb9a29aec3dfc79e863b40ccd
firmanfa <fafirmansyah20@gmail.com>
2022-06-30 21:16:36 +0700

    feat(product-sale-list): add network request for current user

--- app/src/main/java/com/binar/secondhand/kel2/data/repository/ProductSaleListRepository.kt
+++ app/src/main/java/com/binar/secondhand/kel2/data/repository/ProductSaleListRepository.kt
@@ -5,4 +5,6 @@ import com.binar.secondhand.kel2.data.api.service.ApiHelper
 class ProductSaleListRepository(private val apiHelper: ApiHelper) {
 
     suspend fun getSellerProduct() = apiHelper.getSellerProduct()
+
+    suspend fun getAuth() = apiHelper.getAuth()
 }
