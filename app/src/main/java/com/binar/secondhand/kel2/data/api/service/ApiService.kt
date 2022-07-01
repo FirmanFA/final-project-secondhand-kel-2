a3c19194e875167813f95ac2bc29846719647fe7
firmanfa <fafirmansyah20@gmail.com>
2022-07-02 00:02:28 +0700

    fix: error rebase conflict

--- app/src/main/java/com/binar/secondhand/kel2/data/api/service/ApiService.kt
+++ app/src/main/java/com/binar/secondhand/kel2/data/api/service/ApiService.kt
@@ -14,6 +14,7 @@ import com.binar.secondhand.kel2.data.api.model.seller.category.get.GetCategoryR
 import com.binar.secondhand.kel2.data.api.model.buyer.product.GetProductResponse
 import com.binar.secondhand.kel2.data.api.model.buyer.productid.GetProductIdResponse
 import com.binar.secondhand.kel2.data.api.model.buyer.productid.UserProduct
+import com.binar.secondhand.kel2.data.api.model.seller.product.get.GetSellerProductResponse
 import com.binar.secondhand.kel2.data.api.model.seller.product.id.put.PutProductIdRequest
 import com.binar.secondhand.kel2.data.api.model.seller.product.id.put.PutProductIdResponse
 import com.binar.secondhand.kel2.data.api.model.seller.product.post.PostProductRequest
