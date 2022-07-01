279cae597d96ca1fb25eb5bfc0d9536ada0e49bc
firmanfa <fafirmansyah20@gmail.com>
2022-07-01 23:17:57 +0700

    newest commit for rebase

--- app/src/main/java/com/binar/secondhand/kel2/data/api/model/buyer/order/post/PostOrderRequest.kt
+++ app/src/main/java/com/binar/secondhand/kel2/data/api/model/buyer/order/post/PostOrderRequest.kt
@@ -1,11 +1,11 @@
 package com.binar.secondhand.kel2.data.api.model.buyer.order.post
 
-
 import com.google.gson.annotations.SerializedName
 
-data class PostOrderRequest(
+data class PostOrderRequest
+    (
     @SerializedName("product_id")
     val productId: Int,
     @SerializedName("bid_price")
-    val bidPrice: Int
-)
+    val bidPrice: String
+    )
