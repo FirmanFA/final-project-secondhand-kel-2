8fe9bbfe1c47fcd0406f5288c4dfcdd59238b9b5
firmanfa <fafirmansyah20@gmail.com>
2022-07-05 02:37:42 +0700

    feat: add bunch of stuff (losing track of what I am doing)

--- app/src/main/java/com/binar/secondhand/kel2/ui/notification/model.kt
+++
@@ -1,3 +0,0 @@
-//package com.binar.secondhand.kel2.ui.notification
-//
-//data class model (val title, val price, )
---
+++ app/src/main/java/com/binar/secondhand/kel2/ui/sale/sold/SoldProductFragment.kt
@@ -0,0 +1,20 @@
+package com.binar.secondhand.kel2.ui.sale.sold
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.binar.secondhand.kel2.R
+import com.binar.secondhand.kel2.databinding.FragmentSoldProductBinding
+import com.binar.secondhand.kel2.ui.base.BaseFragment
+
+class SoldProductFragment :
+    BaseFragment<FragmentSoldProductBinding>(FragmentSoldProductBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
