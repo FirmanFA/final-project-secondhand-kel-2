5eff7af84e2b55fb648d4671c592b522c8ff9ad5
saddamzikri <msaddamzd@gmail.com>
2022-07-06 19:53:53 +0700

    Adding Splash Screen

---
+++ app/src/main/java/com/binar/secondhand/kel2/ui/splash/SplashFragment.kt
@@ -0,0 +1,42 @@
+package com.binar.secondhand.kel2.ui.splash
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.binar.secondhand.kel2.R
+import com.binar.secondhand.kel2.ui.notification.NotificationViewModel
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            if(onBoardingFinished()){
+                findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+            }else{
+                findNavController().navigate(R.id.action_splashFragment_to_onBoardingFragment)
+            }
+        }, 3000)
+
+    }
+
+    private fun onBoardingFinished(): Boolean{
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return sharedPref.getBoolean("Finished", false)
+    }
+}
