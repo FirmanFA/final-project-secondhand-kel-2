dba6f279321610eaa97cf17f65c282615bb8f9c3
firmanfa <fafirmansyah20@gmail.com>
2022-07-15 20:22:22 +0700

    feat: setup adapter

--- app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeFragment.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeFragment.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.binar.secondhand.kel2.R
 import com.binar.secondhand.kel2.data.api.model.seller.banner.get.GetBannerResponse
@@ -25,6 +28,10 @@ import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.facebook.shimmer.Shimmer
 import com.facebook.shimmer.ShimmerDrawable
 import com.google.android.material.tabs.TabLayout
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.map
 import org.koin.android.ext.android.getKoin
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.text.NumberFormat
@@ -39,7 +46,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
 
         MainFragment.activePage = R.id.main_home
 
-        setUpSearchBarListener()
         setUpObserver()
 
         val token = getKoin().getProperty("access_token", "")
@@ -58,6 +64,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
                 it.findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
             }
         }
+
+//        binding.
 
         homeViewModel.getHomeBanner()
         homeViewModel.getHomeCategory()
@@ -295,32 +303,144 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
             findNavController().navigate(action)
         }
 
-        adapter.submitList(productResponse)
+//        adapter.submitList(productResponse)
 
         binding.rvHomeProduct.adapter = adapter
 
     }
 
-
-    private fun setUpSearchBarListener() {
-//        binding.etSearch.setEndIconOnClickListener {
-//            //do search
-//            searchProduct(binding.etSearch.editText?.text.toString())
-//        }
-//
-//        binding.etSearch.editText?.setOnEditorActionListener { _, i, _ ->
-//
-//            if (i == EditorInfo.IME_ACTION_SEARCH) {
-//                //do something with search
-//                searchProduct(binding.etSearch.editText?.text.toString())
-//            }
-//
-//            true
-//        }
+    private fun FragmentHomeBinding.bindState(
+        uiState: StateFlow<UiState>,
+        pagingData: Flow<PagingData<UiModel>>,
+        uiActions: (UiAction) -> Unit
+    ) {
+        val repoAdapter = HomeProductAdapter {}
+        val header = ProductLoadStateAdapter { repoAdapter.retry() }
+        list.adapter = repoAdapter.withLoadStateHeaderAndFooter(
+            header = header,
+            footer = ReposLoadStateAdapter { repoAdapter.retry() }
+        )
+        bindSearch(
+            uiState = uiState,
+            onQueryChanged = uiActions
+        )
+        bindList(
+            header = header,
+            repoAdapter = repoAdapter,
+            uiState = uiState,
+            pagingData = pagingData,
+            onScrollChanged = uiActions
+        )
     }
 
-    private fun searchProduct(query: String) {
-        //TODO
+    private fun ActivitySearchRepositoriesBinding.bindSearch(
+        uiState: StateFlow<UiState>,
+        onQueryChanged: (UiAction.Search) -> Unit
+    ) {
+        searchRepo.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                updateRepoListFromInput(onQueryChanged)
+                true
+            } else {
+                false
+            }
+        }
+        searchRepo.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                updateRepoListFromInput(onQueryChanged)
+                true
+            } else {
+                false
+            }
+        }
+
+        lifecycleScope.launch {
+            uiState
+                .map { it.query }
+                .distinctUntilChanged()
+                .collect(searchRepo::setText)
+        }
+    }
+
+    private fun ActivitySearchRepositoriesBinding.updateRepoListFromInput(onQueryChanged: (UiAction.Search) -> Unit) {
+        searchRepo.text.trim().let {
+            if (it.isNotEmpty()) {
+                list.scrollToPosition(0)
+                onQueryChanged(UiAction.Search(query = it.toString()))
+            }
+        }
+    }
+
+    private fun ActivitySearchRepositoriesBinding.bindList(
+        header: ReposLoadStateAdapter,
+        repoAdapter: ReposAdapter,
+        uiState: StateFlow<UiState>,
+        pagingData: Flow<PagingData<UiModel>>,
+        onScrollChanged: (UiAction.Scroll) -> Unit
+    ) {
+        retryButton.setOnClickListener { repoAdapter.retry() }
+        list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy != 0) onScrollChanged(UiAction.Scroll(currentQuery = uiState.value.query))
+            }
+        })
+        val notLoading = repoAdapter.loadStateFlow
+            .asRemotePresentationState()
+            .map { it == RemotePresentationState.PRESENTED }
+
+        val hasNotScrolledForCurrentSearch = uiState
+            .map { it.hasNotScrolledForCurrentSearch }
+            .distinctUntilChanged()
+
+        val shouldScrollToTop = combine(
+            notLoading,
+            hasNotScrolledForCurrentSearch,
+            Boolean::and
+        )
+            .distinctUntilChanged()
+
+        lifecycleScope.launch {
+            pagingData.collectLatest(repoAdapter::submitData)
+        }
+
+        lifecycleScope.launch {
+            shouldScrollToTop.collect { shouldScroll ->
+                if (shouldScroll) list.scrollToPosition(0)
+            }
+        }
+
+        lifecycleScope.launch {
+            repoAdapter.loadStateFlow.collect { loadState ->
+                // Show a retry header if there was an error refreshing, and items were previously
+                // cached OR default to the default prepend state
+                header.loadState = loadState.mediator
+                    ?.refresh
+                    ?.takeIf { it is LoadState.Error && repoAdapter.itemCount > 0 }
+                    ?: loadState.prepend
+
+                val isListEmpty = loadState.refresh is LoadState.NotLoading && repoAdapter.itemCount == 0
+                // show empty list
+                emptyList.isVisible = isListEmpty
+                // Only show the list if refresh succeeds, either from the the local db or the remote.
+                list.isVisible =  loadState.source.refresh is LoadState.NotLoading || loadState.mediator?.refresh is LoadState.NotLoading
+                // Show loading spinner during initial load or refresh.
+                progressBar.isVisible = loadState.mediator?.refresh is LoadState.Loading
+                // Show the retry state if initial load or refresh fails.
+                retryButton.isVisible = loadState.mediator?.refresh is LoadState.Error && repoAdapter.itemCount == 0
+                // Toast on any error, regardless of whether it came from RemoteMediator or PagingSource
+                val errorState = loadState.source.append as? LoadState.Error
+                    ?: loadState.source.prepend as? LoadState.Error
+                    ?: loadState.append as? LoadState.Error
+                    ?: loadState.prepend as? LoadState.Error
+                errorState?.let {
+                    Toast.makeText(
+                        this@SearchRepositoriesActivity,
+                        "\uD83D\uDE28 Wooops ${it.error}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
     }
 
 
--- app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeProductAdapter.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeProductAdapter.kt
@@ -1,10 +1,16 @@
 package com.binar.secondhand.kel2.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.PagerAdapter
 import com.binar.secondhand.kel2.data.api.model.buyer.product.GetProductResponseItem
 import com.binar.secondhand.kel2.databinding.HomeProductListLayoutBinding
 import com.bumptech.glide.Glide
@@ -13,19 +19,36 @@ import com.facebook.shimmer.ShimmerDrawable
 import java.text.DecimalFormat
 import java.text.NumberFormat
 
+
 class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) :
-    ListAdapter<GetProductResponseItem, HomeProductAdapter.ViewHolder>(CommunityComparator()) {
+    PagingDataAdapter<UiModel, RecyclerView.ViewHolder>(UIMODEL_COMPARATOR) {
 
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (val uiModel = getItem(position)) {
+            is UiModel.ProductItem -> (holder as RepoViewHolder).bind(uiModel.products)
+            else -> {}
+        }
+    }
 
-    class ViewHolder(private val binding: HomeProductListLayoutBinding) :
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = HomeProductListLayoutBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+
+        return RepoViewHolder(binding)
+    }
+
+    inner class RepoViewHolder(private val binding: HomeProductListLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(
-            currentGetProductResponseItem: GetProductResponseItem,
-            onClick: (GetProductResponseItem) -> Unit
-        ) {
+        private var product: GetProductResponseItem? = null
+
+        fun bind(productResponseItem: GetProductResponseItem) {
+
+            this.product = productResponseItem
+
             binding.root.setOnClickListener {
-                onClick(currentGetProductResponseItem)
+                onClick(productResponseItem)
             }
             val shimmer = Shimmer.AlphaHighlightBuilder()// The attributes for a ShimmerDrawable is set by this builder
                 .setDuration(1800) // how long the shimmering animation takes to do one full sweep
@@ -37,48 +60,100 @@ class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) 
             val shimmerDrawable = ShimmerDrawable().apply {
                 setShimmer(shimmer)
             }
-            Glide.with(binding.root).load(currentGetProductResponseItem.imageUrl)
+            Glide.with(binding.root).load(productResponseItem.imageUrl)
                 .placeholder(shimmerDrawable)
                 .into(binding.imvProductImage)
-            binding.tvProductName.text = currentGetProductResponseItem.name
-            binding.tvProductCategory.text = currentGetProductResponseItem.categories?.joinToString{
+            binding.tvProductName.text = productResponseItem.name
+            binding.tvProductCategory.text = productResponseItem.categories?.joinToString{
                 it.name
             }
             val formatter: NumberFormat = DecimalFormat("#,###")
-            val myNumber = currentGetProductResponseItem.basePrice.toInt()
+            val myNumber = productResponseItem.basePrice.toInt()
             val formattedNumber: String = formatter.format(myNumber).toString()
             //formattedNumber is equal to 1,000,000
             binding.tvProductPrice.text = "Rp. ${formattedNumber}"
         }
-
     }
 
-    class CommunityComparator : DiffUtil.ItemCallback<GetProductResponseItem>() {
-        override fun areItemsTheSame(
-            oldItem: GetProductResponseItem,
-            newItem: GetProductResponseItem
-        ): Boolean {
-            return oldItem === newItem
-        }
+    companion object {
+        private val UIMODEL_COMPARATOR = object : DiffUtil.ItemCallback<UiModel>() {
+            override fun areItemsTheSame(oldItem: UiModel, newItem: UiModel): Boolean {
+                return (oldItem is UiModel.ProductItem && newItem is UiModel.ProductItem &&
+                        oldItem.products.name == newItem.products.name)
+            }
 
-        override fun areContentsTheSame(
-            oldItem: GetProductResponseItem,
-            newItem: GetProductResponseItem
-        ): Boolean {
-            return oldItem.hashCode() == newItem.hashCode()
+            override fun areContentsTheSame(oldItem: UiModel, newItem: UiModel): Boolean =
+                oldItem == newItem
         }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = HomeProductListLayoutBinding.inflate(
-            LayoutInflater.from(parent.context), parent, false
-        )
-
-        return ViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position), onClick)
-    }
-
 }
+
+//class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) :
+//    ListAdapter<GetProductResponseItem, HomeProductAdapter.ViewHolder>(CommunityComparator()) {
+//
+//
+//    class ViewHolder(private val binding: HomeProductListLayoutBinding) :
+//        RecyclerView.ViewHolder(binding.root) {
+//
+//        fun bind(
+//            productResponseItem: GetProductResponseItem,
+//            onClick: (GetProductResponseItem) -> Unit
+//        ) {
+//            binding.root.setOnClickListener {
+//                onClick(productResponseItem)
+//            }
+//            val shimmer = Shimmer.AlphaHighlightBuilder()// The attributes for a ShimmerDrawable is set by this builder
+//                .setDuration(1800) // how long the shimmering animation takes to do one full sweep
+//                .setBaseAlpha(0.7f) //the alpha of the underlying children
+//                .setHighlightAlpha(0.6f) // the shimmer alpha amount
+//                .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+//                .setAutoStart(true)
+//                .build()
+//            val shimmerDrawable = ShimmerDrawable().apply {
+//                setShimmer(shimmer)
+//            }
+//            Glide.with(binding.root).load(productResponseItem.imageUrl)
+//                .placeholder(shimmerDrawable)
+//                .into(binding.imvProductImage)
+//            binding.tvProductName.text = productResponseItem.name
+//            binding.tvProductCategory.text = productResponseItem.categories?.joinToString{
+//                it.name
+//            }
+//            val formatter: NumberFormat = DecimalFormat("#,###")
+//            val myNumber = productResponseItem.basePrice.toInt()
+//            val formattedNumber: String = formatter.format(myNumber).toString()
+//            //formattedNumber is equal to 1,000,000
+//            binding.tvProductPrice.text = "Rp. ${formattedNumber}"
+//        }
+//
+//    }
+//
+//    class CommunityComparator : DiffUtil.ItemCallback<GetProductResponseItem>() {
+//        override fun areItemsTheSame(
+//            oldItem: GetProductResponseItem,
+//            newItem: GetProductResponseItem
+//        ): Boolean {
+//            return oldItem === newItem
+//        }
+//
+//        override fun areContentsTheSame(
+//            oldItem: GetProductResponseItem,
+//            newItem: GetProductResponseItem
+//        ): Boolean {
+//            return oldItem.hashCode() == newItem.hashCode()
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val binding = HomeProductListLayoutBinding.inflate(
+//            LayoutInflater.from(parent.context), parent, false
+//        )
+//
+//        return ViewHolder(binding)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.bind(getItem(position), onClick)
+//    }
+//
+//}
