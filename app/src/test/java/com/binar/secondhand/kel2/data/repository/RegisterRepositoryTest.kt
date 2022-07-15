2bed6ff9194092759947eff5ab31dd351ba6b657
wahyuananta <wahyuanantajr@gmail.com>
2022-07-15 19:53:20 +0700

    Unit test in register repository

---
+++ app/src/test/java/com/binar/secondhand/kel2/data/repository/RegisterRepositoryTest.kt
@@ -0,0 +1,62 @@
+package com.binar.secondhand.kel2.data.repository
+
+import com.binar.secondhand.kel2.data.api.model.auth.login.PostLoginRequest
+import com.binar.secondhand.kel2.data.api.model.auth.login.PostLoginResponse
+import com.binar.secondhand.kel2.data.api.model.auth.register.PostRegisterRequest
+import com.binar.secondhand.kel2.data.api.model.auth.register.PostRegisterResponse
+import com.binar.secondhand.kel2.data.api.service.ApiHelper
+import com.binar.secondhand.kel2.data.api.service.ApiService
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Response
+
+class RegisterRepositoryTest {
+
+    //collaborator
+    private lateinit var apiService: ApiService
+    private lateinit var apiHelper: ApiHelper
+
+    //system under test
+    private lateinit var registerRepository: RegisterRepository
+
+    @Before
+    fun setUp() {
+        apiService = mockk()
+        apiHelper = mockk()
+
+        registerRepository = RegisterRepository(apiHelper)
+    }
+
+    @Test
+    fun postRegister(): Unit = runBlocking {
+        val responsePostRegister = mockk<Response<PostRegisterResponse>>()
+
+        val request = PostRegisterRequest(
+            "Coba",
+            "coba@mail.com",
+            "000000",
+            822233445,
+            "Jakarta Barat",
+            "coba.jpg",
+            "Jakarta")
+
+        every {
+            runBlocking {
+                apiHelper.postRegister(request)
+            }
+        } returns responsePostRegister
+
+        registerRepository.postRegister(request)
+        verify {
+            runBlocking {
+                apiHelper.postRegister(request)
+            }
+        }
+    }
+}
