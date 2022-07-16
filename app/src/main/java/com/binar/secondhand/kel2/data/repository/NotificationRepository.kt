df0344edd4a48ade2657ef3e966046bdec4dd96d
Firmansyah Firdaus Anhar <34387452+FirmanFA@users.noreply.github.com>
2022-07-16 23:49:07 +0700

    Text: Notification ViewModel Unit Test (#69)

--- app/src/main/java/com/binar/secondhand/kel2/data/repository/NotificationRepository.kt
+++ app/src/main/java/com/binar/secondhand/kel2/data/repository/NotificationRepository.kt
@@ -3,5 +3,7 @@ package com.binar.secondhand.kel2.data.repository
 import com.binar.secondhand.kel2.data.api.service.ApiHelper
 
 class NotificationRepository(private val apiHelper: ApiHelper) {
+
     suspend fun getNotification(type: String = "") = apiHelper.getNotification(type)
+
 }
