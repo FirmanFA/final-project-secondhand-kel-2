e480129fa024f04405fe3441f913bfcb11a389b4
firmanfa <fafirmansyah20@gmail.com>
2022-07-16 18:32:44 +0700

    feat: imlpement pagination and offline compability in home product

--- app/src/main/java/com/binar/secondhand/kel2/data/api/model/buyer/product/GetProductResponseItem.kt
+++ app/src/main/java/com/binar/secondhand/kel2/data/api/model/buyer/product/GetProductResponseItem.kt
@@ -2,18 +2,20 @@ package com.binar.secondhand.kel2.data.api.model.buyer.product
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "product")
 data class GetProductResponseItem(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    val productId: Int?=null,
     @field:SerializedName("id")
     val id: Int,
     @field:SerializedName("name")
     val name: String,
     @field:SerializedName("description")
-    val description: String,
+    val description: String?,
     @field:SerializedName("base_price")
     val basePrice: Int,
     @field:SerializedName("image_url")
@@ -21,7 +23,7 @@ data class GetProductResponseItem(
     @field:SerializedName("image_name")
     val imageName: String?,
     @field:SerializedName("location")
-    val location: String,
+    val location: String?,
     @field:SerializedName("user_id")
     val userId: Int,
     @field:SerializedName("status")
@@ -31,10 +33,13 @@ data class GetProductResponseItem(
     @field:SerializedName("updatedAt")
     val updatedAt: String,
     @ColumnInfo(name = "category_string")
-    var categoryString:String,
-    @SerializedName("Categories")
-    val categories: List<Category>?
+    var categoryString:String?,
+
 ) {
+    @Ignore
+    @field:SerializedName("Categories")
+    val categories: List<Category> = emptyList()
+
     data class Category(
         @SerializedName("id")
         val id: Int,
--- app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeProductAdapter.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/home/HomeProductAdapter.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.PagerAdapter
+import com.binar.secondhand.kel2.R
 import com.binar.secondhand.kel2.data.api.model.buyer.product.GetProductResponseItem
 import com.binar.secondhand.kel2.databinding.HomeProductListLayoutBinding
 import com.bumptech.glide.Glide
@@ -21,7 +22,7 @@ import java.text.NumberFormat
 
 
 class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) :
-    PagingDataAdapter<UiModel, RecyclerView.ViewHolder>(UIMODEL_COMPARATOR) {
+    PagingDataAdapter<UiModel.ProductItem, RecyclerView.ViewHolder>(UIMODEL_COMPARATOR) {
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (val uiModel = getItem(position)) {
@@ -62,98 +63,26 @@ class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) 
             }
             Glide.with(binding.root).load(productResponseItem.imageUrl)
                 .placeholder(shimmerDrawable)
+                .error(R.drawable.rectangle_camera)
                 .into(binding.imvProductImage)
             binding.tvProductName.text = productResponseItem.name
-            binding.tvProductCategory.text = productResponseItem.categories?.joinToString{
-                it.name
-            }
+            binding.tvProductCategory.text = productResponseItem.categoryString
             val formatter: NumberFormat = DecimalFormat("#,###")
             val myNumber = productResponseItem.basePrice.toInt()
             val formattedNumber: String = formatter.format(myNumber).toString()
             //formattedNumber is equal to 1,000,000
-            binding.tvProductPrice.text = "Rp. ${formattedNumber}"
+            binding.tvProductPrice.text = "Rp. $formattedNumber"
         }
     }
 
     companion object {
-        private val UIMODEL_COMPARATOR = object : DiffUtil.ItemCallback<UiModel>() {
-            override fun areItemsTheSame(oldItem: UiModel, newItem: UiModel): Boolean {
-                return (oldItem is UiModel.ProductItem && newItem is UiModel.ProductItem &&
-                        oldItem.products.name == newItem.products.name)
+        private val UIMODEL_COMPARATOR = object : DiffUtil.ItemCallback<UiModel.ProductItem>() {
+            override fun areItemsTheSame(oldItem: UiModel.ProductItem, newItem: UiModel.ProductItem): Boolean {
+                return (oldItem.products.name == newItem.products.name)
             }
 
-            override fun areContentsTheSame(oldItem: UiModel, newItem: UiModel): Boolean =
+            override fun areContentsTheSame(oldItem: UiModel.ProductItem, newItem: UiModel.ProductItem): Boolean =
                 oldItem == newItem
         }
     }
 }
-
-//class HomeProductAdapter(private val onClick: (GetProductResponseItem) -> Unit) :
-//    ListAdapter<GetProductResponseItem, HomeProductAdapter.ViewHolder>(CommunityComparator()) {
-//
-//
-//    class ViewHolder(private val binding: HomeProductListLayoutBinding) :
-//        RecyclerView.ViewHolder(binding.root) {
-//
-//        fun bind(
-//            productResponseItem: GetProductResponseItem,
-//            onClick: (GetProductResponseItem) -> Unit
-//        ) {
-//            binding.root.setOnClickListener {
-//                onClick(productResponseItem)
-//            }
-//            val shimmer = Shimmer.AlphaHighlightBuilder()// The attributes for a ShimmerDrawable is set by this builder
-//                .setDuration(1800) // how long the shimmering animation takes to do one full sweep
-//                .setBaseAlpha(0.7f) //the alpha of the underlying children
-//                .setHighlightAlpha(0.6f) // the shimmer alpha amount
-//                .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
-//                .setAutoStart(true)
-//                .build()
-//            val shimmerDrawable = ShimmerDrawable().apply {
-//                setShimmer(shimmer)
-//            }
-//            Glide.with(binding.root).load(productResponseItem.imageUrl)
-//                .placeholder(shimmerDrawable)
-//                .into(binding.imvProductImage)
-//            binding.tvProductName.text = productResponseItem.name
-//            binding.tvProductCategory.text = productResponseItem.categories?.joinToString{
-//                it.name
-//            }
-//            val formatter: NumberFormat = DecimalFormat("#,###")
-//            val myNumber = productResponseItem.basePrice.toInt()
-//            val formattedNumber: String = formatter.format(myNumber).toString()
-//            //formattedNumber is equal to 1,000,000
-//            binding.tvProductPrice.text = "Rp. ${formattedNumber}"
-//        }
-//
-//    }
-//
-//    class CommunityComparator : DiffUtil.ItemCallback<GetProductResponseItem>() {
-//        override fun areItemsTheSame(
-//            oldItem: GetProductResponseItem,
-//            newItem: GetProductResponseItem
-//        ): Boolean {
-//            return oldItem === newItem
-//        }
-//
-//        override fun areContentsTheSame(
-//            oldItem: GetProductResponseItem,
-//            newItem: GetProductResponseItem
-//        ): Boolean {
-//            return oldItem.hashCode() == newItem.hashCode()
-//        }
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        val binding = HomeProductListLayoutBinding.inflate(
-//            LayoutInflater.from(parent.context), parent, false
-//        )
-//
-//        return ViewHolder(binding)
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        holder.bind(getItem(position), onClick)
-//    }
-//
-//}
