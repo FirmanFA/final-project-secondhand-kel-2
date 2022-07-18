14e0900a8c3260bdcf99a9d39a194c5d562e3090
firmanfa <fafirmansyah20@gmail.com>
2022-07-19 02:23:10 +0700

    feat: bottom sheet dialog for select category

--- app/src/main/java/com/binar/secondhand/kel2/ui/lengkapi/CategoryBottomDialog.kt
+++ app/src/main/java/com/binar/secondhand/kel2/ui/lengkapi/CategoryBottomDialog.kt
@@ -1,17 +1,94 @@
 package com.binar.secondhand.kel2.ui.lengkapi
 
-import com.binar.secondhand.kel2.data.api.model.seller.category.get.GetCategoryResponse
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.FrameLayout
+import androidx.core.widget.addTextChangedListener
+import com.binar.secondhand.kel2.R
 import com.binar.secondhand.kel2.data.api.model.seller.category.get.GetCategoryResponseItem
 import com.binar.secondhand.kel2.databinding.BottomDialogCategoryBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.util.*
 
 class CategoryBottomDialog(
-    listCategory: List<GetCategoryResponseItem>
-): BottomSheetDialogFragment() {
+    private val listCategory: ArrayList<Pair<Boolean, GetCategoryResponseItem>>,
+    private val onSave: (ArrayList<GetCategoryResponseItem>) -> Unit
+) : BottomSheetDialogFragment() {
 
 //    private lateinit var binding:
 
     private lateinit var binding: BottomDialogCategoryBinding
+    private val listSelectedCategory = ArrayList<GetCategoryResponseItem>()
+    private lateinit var adapter: SelectCategoryAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = BottomDialogCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun ChipGroup.addChip(category: GetCategoryResponseItem, chipId: Int) {
+
+        Chip(context).apply {
+            id = chipId
+            text = category.name
+            isClickable = true
+            isCloseIconVisible = true
+            setOnCloseIconClickListener {
+                listCategory.add(chipId, Pair(false, category))
+                adapter.notifyItemChanged(chipId)
+                removeView(it)
+            }
+            addView(this)
+
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val chipGroup = binding.chipGroupSelectedCategory
+
+
+
+        adapter = SelectCategoryAdapter { category, isChecked, pos ->
+            if (isChecked) {
+                listSelectedCategory.add(category)
+                chipGroup.addChip(category, pos)
+            } else {
+                listSelectedCategory.remove(category)
+                val chip = view.findViewById<Chip>(pos)
+                chipGroup.removeView(chip)
+            }
+        }
+        adapter.submitData(listCategory)
+        binding.rvSelectCategory.adapter = adapter
+
+        binding.btnSave.setOnClickListener {
+            onSave(listSelectedCategory)
+        }
+
+        binding.etSearch.editText?.addTextChangedListener { keyword ->
+            val filteredList =
+                listCategory.filter {
+                    it.second.name.lowercase().contains(keyword.toString().lowercase())
+                }
+
+            adapter.submitData(filteredList)
+        }
+
+    }
 
 
 }
---
+++ app/src/main/java/com/binar/secondhand/kel2/ui/lengkapi/SelectCategoryAdapter.kt
@@ -0,0 +1,57 @@
+package com.binar.secondhand.kel2.ui.lengkapi
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.binar.secondhand.kel2.data.api.model.seller.category.get.GetCategoryResponseItem
+import com.binar.secondhand.kel2.databinding.SelectCategoryListLayoutBinding
+
+class SelectCategoryAdapter(private val onClick: (GetCategoryResponseItem,Boolean, Int) -> Unit) :
+    RecyclerView.Adapter<SelectCategoryAdapter.ViewHolder>() {
+
+    private val diffCallback =
+        object : DiffUtil.ItemCallback<Pair<Boolean,GetCategoryResponseItem>>() {
+            override fun areItemsTheSame(
+                oldItem: Pair<Boolean,GetCategoryResponseItem>,
+                newItem: Pair<Boolean,GetCategoryResponseItem>
+            ): Boolean = oldItem.second.id == newItem.second.id
+
+            override fun areContentsTheSame(
+                oldItem: Pair<Boolean,GetCategoryResponseItem>,
+                newItem: Pair<Boolean,GetCategoryResponseItem>
+            ): Boolean = oldItem.hashCode() == newItem.hashCode()
+        }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    fun submitData(value: List<Pair<Boolean,GetCategoryResponseItem>>?) =
+        differ.submitList(value)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SelectCategoryAdapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(SelectCategoryListLayoutBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: SelectCategoryAdapter.ViewHolder, position: Int) {
+        val data = differ.currentList[position]
+        data.let { holder.bind(data, position) }
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    inner class ViewHolder(private val binding: SelectCategoryListLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(category: Pair<Boolean,GetCategoryResponseItem>, position: Int) {
+            binding.cbCategory.setOnCheckedChangeListener { _, isChecked ->
+                onClick(category.second, isChecked, position)
+            }
+            binding.cbCategory.text = category.second.name
+            binding.cbCategory.isChecked = category.first
+        }
+    }
+}
