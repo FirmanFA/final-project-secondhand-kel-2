2adf066708d5fe3857f266b767e788ca9d39e90a
Wahyu Ananta Kuharian <79406579+wahyuananta@users.noreply.github.com>
2022-07-19 07:48:12 +0700

    Merge pull request #82
    * Unit test in repository
    
    * Unit test in UI account view model

---
+++ app/src/test/java/com/binar/secondhand/kel2/ui/account/AccountViewModelTest.kt
@@ -0,0 +1,57 @@
+package com.binar.secondhand.kel2.ui.account
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.binar.secondhand.kel2.data.api.model.auth.user.GetAuthResponse
+import com.binar.secondhand.kel2.data.api.model.notification.GetNotificationResponse
+import com.binar.secondhand.kel2.data.repository.NotificationRepository
+import com.binar.secondhand.kel2.data.repository.Repository
+import com.binar.secondhand.kel2.rule.MainCoroutineRule
+import com.binar.secondhand.kel2.ui.notification.NotificationViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mockito
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import retrofit2.Response
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class AccountViewModelTest {
+
+    private lateinit var viewModel: AccountViewModel
+    private lateinit var repository: Repository
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        repository = mock()
+        viewModel = AccountViewModel(repository)
+    }
+
+    @Test
+    fun getAuth()= runTest{
+        val authGetResponse = mock<Response<GetAuthResponse>>()
+
+        given(repository.getAuth()).willReturn(authGetResponse)
+
+        viewModel.getAuth()
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).getAuth()
+        kotlin.test.assertNotNull(viewModel.authGetResponse.value?.status)
+        kotlin.test.assertEquals(viewModel.authGetResponse.value?.data, authGetResponse)
+    }
+}
