cf27980d992cf222772f031acfb21cbd1ab7d43f
firmanfa <fafirmansyah20@gmail.com>
2022-07-20 19:13:45 +0700

    fix: buyer/product api response changed

--- app/src/main/java/com/binar/secondhand/kel2/data/api/model/buyer/product/GetProductResponse.kt
+++ app/src/main/java/com/binar/secondhand/kel2/data/api/model/buyer/product/GetProductResponse.kt
@@ -3,12 +3,4 @@ package com.binar.secondhand.kel2.data.api.model.buyer.product
 
 import com.google.gson.annotations.SerializedName
 
-data class GetProductResponse(
-    @SerializedName("page")
-    val page: Int,
-    @SerializedName("per_page")
-    val perPage: Int,
-    @SerializedName("data")
-    val productResponseItem: List<GetProductResponseItem> = emptyList(),
-    val nextPage: Int? = null
-)
+class GetProductResponse: ArrayList<GetProductResponseItem>()
