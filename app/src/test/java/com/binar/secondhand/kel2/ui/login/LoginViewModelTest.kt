a01f37f7bb0b61f3fd22b514bf4151c2d23c331b
Wahyu Ananta Kuharian <79406579+wahyuananta@users.noreply.github.com>
2022-07-22 09:26:52 +0700

    Merge pull request #101
    * Unit test in repository
    
    * Unit test in UI account view model
    
    * Unit test in UI bidder view model
    
    * Unit test in UI detail product view model
    
    * Unit test in UI edit view model
    
    * Unit test in UI home view model
    
    * Update unit test in UI home view model
    
    * Unit test in UI seller detail product view model
    
    * Unit test in UI login view model

---
+++ app/src/test/java/com/binar/secondhand/kel2/ui/login/LoginViewModelTest.kt
@@ -0,0 +1,59 @@
+package com.binar.secondhand.kel2.ui.login
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.binar.secondhand.kel2.data.api.model.auth.login.PostLoginRequest
+import com.binar.secondhand.kel2.data.api.model.auth.login.PostLoginResponse
+import com.binar.secondhand.kel2.data.api.model.seller.order.SellerOrderIdResponse
+import com.binar.secondhand.kel2.data.repository.LoginRepository
+import com.binar.secondhand.kel2.data.repository.Repository
+import com.binar.secondhand.kel2.rule.MainCoroutineRule
+import com.binar.secondhand.kel2.ui.lengkapi.SellerDetailProductViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mockito
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import retrofit2.Response
+
+class LoginViewModelTest {
+
+    private lateinit var viewModel: LoginViewModel
+    private lateinit var repository: LoginRepository
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        repository = mock()
+        viewModel = LoginViewModel(repository)
+    }
+
+    @Test
+    fun postLogin()= runTest{
+        val postLoginGetResponse = mock<Response<PostLoginResponse>>()
+
+        val request = PostLoginRequest("johnde@mail.com", "123456")
+
+        given(repository.postLogin(request)).willReturn(postLoginGetResponse)
+
+        viewModel.postLogin(request)
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).postLogin(request)
+        kotlin.test.assertNotNull(viewModel.loginPostResponse)
+        kotlin.test.assertEquals(viewModel.loginPostResponse.value?.data, postLoginGetResponse)
+    }
+}
