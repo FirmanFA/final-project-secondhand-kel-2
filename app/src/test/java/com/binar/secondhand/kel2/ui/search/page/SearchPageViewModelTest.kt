36d43b343e603e440efc91c8490d0bf96dadb52c
Wahyu Ananta Kuharian <79406579+wahyuananta@users.noreply.github.com>
2022-07-23 10:52:19 +0700

    Merge pull request #110
    * Unit test in repository
    
    * Unit test in UI account view model
    
    * Unit test in UI bidder view model
    
    * Unit test in UI detail product view model
    
    * Unit test in UI edit view model
    
    * Unit test in UI home view model
    
    * Update unit test in UI home view model
    
    * Unit test in UI seller detail product view model
    
    * Unit test in UI login view model
    
    * Unit test in UI main view model
    
    * Unit test in UI change pass view model
    
    * Unit test in UI preview view model
    
    * Update unit test in UI change pass view model
    
    * Unit test in UI profile view model
    
    * Unit test in UI register view model
    
    * Unit test in UI product sale list view model
    
    * Unit test in UI search view model

---
+++ app/src/test/java/com/binar/secondhand/kel2/ui/search/page/SearchPageViewModelTest.kt
@@ -0,0 +1,73 @@
+package com.binar.secondhand.kel2.ui.search.page
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.binar.secondhand.kel2.data.api.model.wishlist.get.GetWishlist
+import com.binar.secondhand.kel2.data.local.room.model.SearchHistoryEntity
+import com.binar.secondhand.kel2.data.repository.Repository
+import com.binar.secondhand.kel2.data.repository.SearchHistoryRepository
+import com.binar.secondhand.kel2.rule.MainCoroutineRule
+import com.binar.secondhand.kel2.ui.wishlist.WishlistViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mockito
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import retrofit2.Response
+
+class SearchPageViewModelTest {
+
+    private lateinit var viewModel: SearchPageViewModel
+    private lateinit var repository: SearchHistoryRepository
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        repository = mock()
+        viewModel = SearchPageViewModel(repository)
+    }
+
+    @Test
+    fun getSearchHistory()= runTest{
+        val searchHistoryEntity = mock<List<SearchHistoryEntity>>()
+
+        given(repository.getSearchHistory()).willReturn(searchHistoryEntity)
+
+        viewModel.getSearchHistory()
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).getSearchHistory()
+        kotlin.test.assertNotNull(viewModel.searchResponse)
+        kotlin.test.assertEquals(viewModel.searchResponse.value?.data, searchHistoryEntity)
+    }
+
+    @Test
+    fun insertSearchHistory()= runTest{
+
+        val searchHistoryEntity = SearchHistoryEntity(
+            1,
+            "Pakaian"
+        )
+
+        given(repository.insertSearchHistory(searchHistoryEntity))
+
+        viewModel.insertSearchHistory(searchHistoryEntity)
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).insertSearchHistory(searchHistoryEntity)
+    }
+}
