24d77e51b588a8a4ba5aab5e38be82b0354ab16e
Wahyu Ananta Kuharian <79406579+wahyuananta@users.noreply.github.com>
2022-07-23 10:29:07 +0700

    Merge pull request #109
    * Unit test in repository
    
    * Unit test in UI account view model
    
    * Unit test in UI bidder view model
    
    * Unit test in UI detail product view model
    
    * Unit test in UI edit view model
    
    * Unit test in UI home view model
    
    * Update unit test in UI home view model
    
    * Unit test in UI seller detail product view model
    
    * Unit test in UI login view model
    
    * Unit test in UI main view model
    
    * Unit test in UI change pass view model
    
    * Unit test in UI preview view model
    
    * Update unit test in UI change pass view model
    
    * Unit test in UI profile view model
    
    * Unit test in UI register view model
    
    * Unit test in UI product sale list view model

---
+++ app/src/test/java/com/binar/secondhand/kel2/ui/sale/main/ProductSaleListViewModelTest.kt
@@ -0,0 +1,103 @@
+package com.binar.secondhand.kel2.ui.sale.main
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.binar.secondhand.kel2.data.api.model.auth.user.GetAuthResponse
+import com.binar.secondhand.kel2.data.api.model.notification.GetNotificationResponse
+import com.binar.secondhand.kel2.data.api.model.seller.order.GetOrderResponse
+import com.binar.secondhand.kel2.data.api.model.seller.product.get.GetSellerProductResponse
+import com.binar.secondhand.kel2.data.repository.ProductSaleListRepository
+import com.binar.secondhand.kel2.data.repository.RegisterRepository
+import com.binar.secondhand.kel2.rule.MainCoroutineRule
+import com.binar.secondhand.kel2.ui.register.RegisterViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mockito
+import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
+import retrofit2.Response
+
+class ProductSaleListViewModelTest {
+
+    private lateinit var viewModel: ProductSaleListViewModel
+    private lateinit var repository: ProductSaleListRepository
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        repository = mock()
+        viewModel = ProductSaleListViewModel(repository)
+    }
+
+    @Test
+    fun getSellerProduct()= runTest{
+        val sellerProductGetResponse = mock<Response<GetSellerProductResponse>>()
+
+        given(repository.getSellerProduct()).willReturn(sellerProductGetResponse)
+
+        viewModel.getSellerProduct()
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).getSellerProduct()
+        kotlin.test.assertNotNull(viewModel.getSellerProductResponse)
+        kotlin.test.assertEquals(viewModel.getSellerProductResponse.value?.data, sellerProductGetResponse)
+    }
+
+    @Test
+    fun getAuth()= runTest{
+        val authGetResponse = mock<Response<GetAuthResponse>>()
+
+        given(repository.getAuth()).willReturn(authGetResponse)
+
+        viewModel.getAuth()
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).getAuth()
+        kotlin.test.assertNotNull(viewModel.authGetResponse)
+        kotlin.test.assertEquals(viewModel.authGetResponse.value?.data, authGetResponse)
+    }
+
+    @Test
+    fun getNotification()= runTest{
+        val orderResponse = mock<Response<GetOrderResponse>>()
+
+        given(repository.getSellerOrder()).willReturn(orderResponse)
+
+        viewModel.getNotification()
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).getSellerOrder()
+        kotlin.test.assertNotNull(viewModel.notificationResponse.value?.status)
+        kotlin.test.assertEquals(viewModel.notificationResponse.value?.data, orderResponse)
+    }
+
+    @Test
+    fun deleteProduct()= runTest{
+        val unit = mock<Response<Unit>>()
+
+        given(repository.deleteSellerProductId(1)).willReturn(unit)
+
+        viewModel.deleteProduct(1)
+
+        advanceUntilIdle()
+
+        Mockito.verify(repository, Mockito.times(1)).deleteSellerProductId(1)
+        kotlin.test.assertNotNull(viewModel.deleteProduct)
+        kotlin.test.assertEquals(viewModel.deleteProduct.value?.data, unit)
+    }
+}
